6dddad471ca00bc4ccbb5cf264ee897b72cb2411
Pemmy0 <pemmylicious00@gmail.com>
2025-01-02 14:06:42 +0700

    Character Movement

--- Player/drago.gd
+++ Player/drago.gd
@@ -1,12 +1,13 @@
 extends CharacterBody2D
 
-
-const SPEED = 50.0
-
 @onready var animated_sprite_2d = $AnimatedSprite2D
 
+var speed = 25
+var movement_animation: String
+
 func _physics_process(delta):
-	var direction = Input.get_axis("ui_left", "ui_right")
+	var direction = Input.get_axis("Left", "Right")
+	movement_animation = "Walk"
 	
 	movement(direction)
 	animation_control(direction)
@@ -15,11 +16,23 @@ func _physics_process(delta):
 
 func movement(direction):
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x = direction * speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, speed)
+		
+	if Input.is_action_pressed("Run"):
+		speed = 50
+		movement_animation = "Run"
+	else:
+		movement_animation = "Walk"
+		speed = 25
 		
 func animation_control(direction):
+	if direction != 0: 
+		animated_sprite_2d.play(movement_animation)
+	elif velocity.x == 0:
+		animated_sprite_2d.play("Idle")
+	
 	if direction == 1:
 		animated_sprite_2d.flip_h = false
 	elif direction == -1:
