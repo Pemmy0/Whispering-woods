29eb8a7a9bbbd12a9c2210a58d42867c01c5b2a7
Pemmy0 <pemmylicious00@gmail.com>
2025-01-02 16:27:53 +0700

    Updated Flashlight Texture

--- Singletons/game_manager.gd
+++ Singletons/game_manager.gd
@@ -3,9 +3,8 @@ extends Node
 var isFullscreen : bool = false
 
 func _ready() -> void:
-	DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN, 0)
-	isFullscreen = true
-
+	DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED, 0)
+	isFullscreen = false
 func _process(_delta: float) -> void:
 	if(Input.is_action_just_pressed("Fullscreen")):
 		if(isFullscreen):
